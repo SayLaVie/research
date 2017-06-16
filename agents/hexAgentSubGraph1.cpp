69d5600f46ca9a0e13a7af9e3eae6f9378f4b486
SayLaVie <mmccarver@angelo.edu>
2017-06-15 22:50:48 -0500

    Added previous attempt at subgraph idea

---
+++ agents/hexAgentSubGraph1.cpp
@@ -0,0 +1,205 @@
+#include "hex.h"
+#include <queue>
+
+int hexAgentSubGraph1MinMax(Board board, player whichPlayer, int depth, vector<int> moveList);
+
+// priority_queue<int> hexAgentSubGraph1MoveList;
+
+int hexAgentSubGraph1 (const Board &board, player whichPlayer)
+{
+   int subGraphSize, boardSize, row, column, moveToAdd, tmpRow, tmpCol;
+   Board *subGraph;
+   vector<Tile> subTiles;
+   vector<int> moveList;
+   player mover;
+
+   boardSize = board.getSize();
+   subGraphSize = 3;
+// cout << board.getBoard()[0].getOwner() << endl;
+   do
+   {
+      // Break board into all subgraphs, evaluating each one and pushing
+      // moves onto priority_queue based on heuristic
+      for (row = 0; row <= boardSize - subGraphSize; ++row)
+      {
+         for (column = 0; column <= boardSize - subGraphSize; ++column)
+         {
+            subGraph = new Board(subGraphSize);
+
+            // Two inner loops to push tiles onto vector of tiles
+            for (int subRow = row; subRow < row + subGraphSize; ++subRow)
+            {
+               for (int subCol = column; subCol < column + subGraphSize; ++subCol)
+               {
+                  if (board.getATurn() == 2 && boardSize == 4 && whichPlayer == playerA)
+                     cout << endl << boardSize * subRow + subCol;
+                  subTiles.push_back(board.getBoard()[boardSize * subRow + subCol]);
+               }
+            }
+
+            // Use vector of tiles to create subGraph
+            for (int i = 0; i < subGraphSize * subGraphSize; ++i)
+            {
+               mover = subTiles[i].getOwner();
+
+               if (mover != none)
+               {
+                  subGraph->makeMove(i, mover);
+               }
+
+               subTiles.clear();
+            }
+
+            // We now have a subGraph. Evaluate it
+            moveToAdd = hexAgentSubGraph1MinMax(*subGraph, whichPlayer, 0, moveList);
+
+            // Turn this location into a location for a board the next size up, if it was a good one
+            if (moveToAdd != -1)
+            {
+               // Turn location into row and column for this size board
+               tmpRow = moveToAdd / subGraphSize;
+               tmpCol = moveToAdd % subGraphSize;
+
+               // Turn these numbers into a location for a board 1 size bigger
+               tmpRow += row;
+               tmpCol += column;
+
+               // cout << endl << "row " << tmpRow << " col " << tmpCol;
+
+               // Push onto move vector
+               // cout << endl << "size " << subGraphSize << " currentCol: " << column << " move " << moveToAdd << " " << (subGraphSize + (subGraphSize == boardSize ? 0 : 1)) * tmpRow + tmpCol;
+
+               if (subGraphSize < boardSize)
+               {
+                  moveList.push_back((subGraphSize + 1) * tmpRow + tmpCol);
+               }
+               else
+               {
+                  moveList.push_back(subGraphSize * tmpRow + tmpCol);
+               }
+            }
+
+            delete subGraph;
+
+         }
+      }
+
+      subGraphSize += 1;
+
+   } while (subGraphSize <= boardSize);
+
+   if (moveList.size() > 0)
+    {//cout << endl << "play " << moveList.back() << endl;;
+      return moveList.back();
+   }
+
+   return 0;
+}
+
+int hexAgentSubGraph1MinMax(Board board, player whichPlayer, int depth, vector<int> moveList)
+{
+   int location, boardSize, value, minMax, bestMoveSoFar;
+   vector<int> copyList, notOnList;
+   Board copyBoard;
+
+   boardSize = board.getSize();
+
+   if (board.isGameOver())
+   {
+      if (depth % 2 == 0)
+      {
+         return -1;
+      }
+
+      return 1;
+   }
+
+   // Try out the best moves from before
+   if (boardSize != 3)
+   {
+      while (!copyList.empty())
+      {
+         bestMoveSoFar = copyList.back();
+         copyList.pop_back();
+
+         if (board.isValidMove(bestMoveSoFar))
+         {
+            copyBoard = board;
+            copyBoard.makeMove(bestMoveSoFar, whichPlayer);
+
+            minMax = hexAgentSubGraph1MinMax(copyBoard, whichPlayer == playerA ? playerB : playerA, depth + 1, copyList);
+
+            if (depth % 2 == 0)
+            {
+               if (minMax == 1)
+               {
+                  if (depth == 0)
+                  {
+                     return bestMoveSoFar;
+                  }
+
+                  return 1;
+               }
+            }
+            else
+            {
+               if (minMax == -1)
+               {
+                  return -1;
+               }
+            }
+         }
+      }
+
+      if (depth == 0)
+      {
+         return -1;//bestMoveSoFar;
+      }
+
+      return minMax;
+   }
+
+   else
+   {
+      for (location = 0; location < boardSize * boardSize; ++location)
+      {
+         if (board.isValidMove(location))
+         {
+            copyBoard = board;
+
+            copyBoard.makeMove(location, whichPlayer);
+
+            minMax = hexAgentSubGraph1MinMax(copyBoard, whichPlayer == playerA ? playerB : playerA, depth + 1, moveList);
+
+            if (depth % 2 == 0)
+            {
+               if (minMax == 1)
+               {
+                  if (depth == 0)
+                  {
+                     return location;
+                  }
+
+                  return 1;
+               }
+            }
+            else
+            {
+               if (minMax == -1)
+               {
+                  return -1;
+               }
+            }
+         }
+      }
+
+      if (depth == 0)
+      {
+         return -1;//bestMoveSoFar;
+      }
+
+      return minMax;
+   }
+
+   return -1;
+}
