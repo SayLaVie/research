e6800ea84ad3d0cada03da1af49873773d973fb8
SayLaVie <mmccarver27@gmail.com>
2017-09-19 00:56:01 -0500

    work on net weights

--- src/hexWorld.cpp
+++ src/hexWorld.cpp
@@ -10,9 +10,32 @@ advisor: Dr. Rob LeGrand
 // This function sets the weights for the neural net
 void hexWorld::nextGeneration()
 {
+	// This array sets the shape of the neural net
+	// Each entry is the depth of each consecutive layer
+	vector<int> netShape;
+	// This vector of vectors of vectors keeps track of the weights
+	vector<vector<vector<int> > > netWeights;
+	int layer, row, player;
+
+	// Push the depths of each layer into the netShape vector
+	// Not sure yet what our first network shape will be,
+	// setting it as <3, 2, 1> for now.
+	netShape.push_back(3);
+	netShape.push_back(2);
+	netShape.push_back(1);
+
 	// Check if this is the first generation (make randomized weights)
 	if (hexGamePlayers.size() == 0)
 	{
+		// Create a skeleton vector for the shape of the neural net
+		for (layer = 0; layer < netShape.size() - 1; ++layer)
+		{
+			for (row = 0; row < netShape[layer]; ++row)
+			{
+
+			}
+		}
+
 		for (int player = 0; player < NUM_PLAYERS; ++player)
 		{
 			hexGamePlayers.push_back(hexGamePlayer(/*random weights*/));
