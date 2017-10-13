0d9eca2a905fbb2e61c68a3926df92be00916729
SayLaVie <mmccarver@angelo.edu>
2017-10-13 17:33:15 -0500

    Checking in helpers

---
+++ src/hexEvolutionHelpers.cpp
@@ -0,0 +1,201 @@
+#include <hexEvolutionHelpers.h>
+
+void printUsage(int exitCode)
+{
+	cerr << "Usage: hexEvolution <option(s)>" << endl;
+	cerr << "Options:" << endl;
+	cerr << "\t-f,--frequency\t\tSpecify how frequently hexWorld data should be saved (default: after every 10 iterations)" << endl;
+	cerr << "\t-h,--help\t\tShow this message" << endl;
+	cerr << "\t-i,--iterations\t\tSpecify number of iterations to run (default: 10)" << endl;
+	cerr << "\t-o,--output\t\tSpecify full or relative path of the file name to output results to (default: results/results.out)" << endl;
+	cerr << "\t-r,--resume\t\tSpecify the full or relative path of the file name containing hexWorld data (usually found in data/ subdirectories)" << endl;
+	
+	exit(exitCode);
+}
+
+bool isNumeric(string input)
+{
+	for (int character = 0; character < input.length(); character += 1)
+	{
+		if (!isdigit(input[character]))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+player playHexGame(hexGamePlayer hexPlayerA, hexGamePlayer hexPlayerB)
+{
+	int playerMove, numberOfTurns;
+	player currentPlayer;
+	Board board(BOARD_SIZE);
+
+	numberOfTurns = 0;
+
+	while (!board.isGameOver())
+	{
+		currentPlayer = static_cast<player>(numberOfTurns % 2);
+
+		if (currentPlayer == playerA)
+		{
+			playerMove = hexPlayerA.play(board, playerA);
+		}
+		else
+		{
+			playerMove = hexPlayerB.play(board, playerB);
+		}
+
+		// Put an 'if not valid move' conditional here
+
+		board.makeMove(playerMove, currentPlayer);
+
+		numberOfTurns += 1;
+	}
+
+	// The most recent value of currentPlayer is the player who won
+	return currentPlayer;
+}
+
+void playHexGames(hexWorld &population, ofstream &fout)
+{
+	int playerLocation, currentNeighbor;
+	player gameWinner;
+	vector<int> neighboringPlayers;
+
+	for (playerLocation = 0; playerLocation < population.getNumPlayers(); playerLocation += 1)
+	{
+		neighboringPlayers.clear();
+		neighboringPlayers = population.getNeighbors(playerLocation);
+
+		/**
+		Testing Area
+		**/
+		// fout << "\tPlayer" << playerLocation << " vs:" << endl;
+
+		for (currentNeighbor = 0; currentNeighbor < neighboringPlayers.size(); currentNeighbor += 1)
+		{
+			// fout << "\t\t" << neighboringPlayers[currentNeighbor] << ": ";
+			gameWinner = playHexGame(population.getHexGamePlayer(playerLocation), population.getHexGamePlayer(neighboringPlayers[currentNeighbor]));
+
+			if (gameWinner == playerA)
+			{
+				// fout << "Win" << endl;
+				population.addPlayerWin(playerLocation);
+			}
+			else
+			{
+				// fout << "Loss" << endl;
+				population.addPlayerWin(neighboringPlayers[currentNeighbor]);
+			}
+		}
+	}
+}
+
+hexWorld entirePopulationFileParser(ifstream &fin)
+{
+	int numGamesWon;
+	double weight;
+	string parserGuide, line, token, delim;
+	size_t pos;
+	vector<double> singleInputNodeVector;
+	vector<vector<double> > vectorOfInputNodes;
+	vector<vector<vector<double> > > vectorOfLayers;
+	vector<hexGamePlayer> hexGamePlayers;
+	hexWorld population;
+
+	hexGamePlayers.clear();
+	delim = ",";
+
+	while (fin >> parserGuide)
+	{
+		// Add netShape variable and code
+		// Add numPlayers variable and code
+
+
+		// numGamesWon stat for hexGamePlayer
+		if (parserGuide == "stat")
+		{
+			fin >> numGamesWon;
+		}
+		// Beginning of a new layer
+		else if (parserGuide == "layer")
+		{
+			if (vectorOfInputNodes.size() == 0)
+			{
+				// This is the beginning of the file, and there's nothing to push back
+				vectorOfLayers.clear();
+				vectorOfInputNodes.clear();
+			}
+			else
+			{
+				vectorOfLayers.push_back(vectorOfInputNodes);
+				vectorOfInputNodes.clear();
+			}
+		}
+		// Following line content is a vector of weights (one single node)
+		else if (parserGuide == "node")
+		{
+			// The rest of the line should be double values seperated by commas
+			getline(fin, line);
+			singleInputNodeVector.clear();
+
+			while (line.length() > 0)
+			{
+				pos = line.find(delim);
+				token = line.substr(0, pos);
+				
+				weight = atof(token.c_str());
+				singleInputNodeVector.push_back(weight);
+
+				line.erase(0, pos + 1);
+			}
+
+			vectorOfInputNodes.push_back(singleInputNodeVector);
+		}
+		// End of a single player's input
+		else if (parserGuide == "endPlayer")
+		{
+			vectorOfLayers.push_back(vectorOfInputNodes);
+			hexGamePlayers.push_back(hexGamePlayer(vectorOfLayers, numGamesWon));
+
+			vectorOfInputNodes.clear();
+			vectorOfLayers.clear();
+		}
+		// Comment symbol ignores the rest of the line
+		else if (parserGuide == "//")
+		{
+			getline(fin, line);
+		}
+		// Invalid syntax in file
+		else
+		{
+			cout << "Invalid symbol found in file: " << parserGuide << endl;
+			exit(0);
+		}
+	}
+
+	population = hexWorld(hexGamePlayers);
+
+	hexPopulationSanitation(population);
+
+	return population;
+}
+
+void hexPopulationSanitation(hexWorld population)
+{
+	// No sanitation yet
+}
+
+void printCurrentGenerationToFile(hexWorld population, ofstream &fout)
+{
+	int player;
+
+	for (player = 0; player < population.getNumPlayers(); player += 1)
+	{
+		fout << "// Player" << player << endl;
+
+		population.getHexGamePlayer(player).printWeights(fout);
+	}
+}
---
+++ src/hexHumanVsBotHelpers.cpp
@@ -0,0 +1,166 @@
+#include <hexHumanVsBotHelpers.h>
+
+void printUsage(int exitCode)
+{
+	cerr << "Usage: hexHumanVsBot <option>" << endl;
+	cerr << "Options:" << endl;
+	cerr << "\t-h,--help\t\tShow this message" << endl;
+	cerr << "\t-p,--player\t\tSpecify the relative or full path name of a file that contains the data for a hexPlayer" << endl;
+
+	exit(exitCode);
+}
+
+ 
+// Takes ifstream and parses nerualNetWeights 3-D vector from file.
+vector<vector<vector<double> > > singleNeuralNetFileParser(ifstream &fin)
+{
+	double weight;
+	string parserGuide, line, token, delim;
+	size_t pos;
+	vector<int> netShape;
+	vector<double> singleInputNodeVector;
+	vector<vector<double> > vectorOfInputNodes;
+	vector<vector<vector<double> > > vectorOfLayers;
+
+	delim = ",";
+
+	while (fin >> parserGuide)
+	{
+		if (parserGuide == "layer" || parserGuide == "endPlayer")
+		{
+			if (vectorOfInputNodes.size() == 0)
+			{
+				// This is the beginning of the file, and there's nothing to push back
+				vectorOfLayers.clear();
+				vectorOfInputNodes.clear();
+			}
+			else
+			{
+				vectorOfLayers.push_back(vectorOfInputNodes);
+				vectorOfInputNodes.clear();
+			}
+		}
+		else if (parserGuide == "node")
+		{
+			// The rest of the line should be double values seperated by comas
+			getline(fin, line);
+			singleInputNodeVector.clear();
+
+			while (line.length() > 0)
+			{
+				pos = line.find(delim);
+				token = line.substr(0, pos);
+				
+				weight = atof(token.c_str());
+				singleInputNodeVector.push_back(weight);
+
+				line.erase(0, pos + 1);
+			}
+
+			vectorOfInputNodes.push_back(singleInputNodeVector);
+		}
+		else if (parserGuide == "stat")
+		{
+			// Ignore numGamesWon (the next input)
+			fin >> parserGuide;
+		}
+		else if (parserGuide == "//")
+		{
+			// Ignore the rest of the line
+			getline(fin, line);
+		}
+		else
+		{
+			cout << "Invalid symbol found in file" << endl;
+			exit(0);
+		}
+	}
+
+	return vectorOfLayers;
+}
+
+void playHexGame(hexGamePlayer bot)
+{
+	Board board(BOARD_SIZE);
+	string answer, move;
+	int row, column, playerMove, turnNumber;
+	bool answerQuestion, madeMove;
+	player currentPlayer, humanPlayer;
+
+	answerQuestion = false;
+
+	while (!answerQuestion)
+	{
+		cout << "Would you like to play first (Y/N)? ";
+		cin >> answer;
+
+		if (answer.length() != 1 || (toupper(answer[0]) != 'Y' && toupper(answer[0]) != 'N'))
+		{
+			cout << "Invalid choice" << endl;
+		}
+		else
+		{
+			answerQuestion = true;
+		}
+	}
+
+	if (toupper(answer[0]) == 'Y')
+	{
+		humanPlayer = playerA;
+	}
+	else
+	{
+		humanPlayer = playerB;
+	}
+
+	turnNumber = 0;
+
+	cout << endl << "Try to connect the left and right sides!" << endl;
+	cout << "Enter your move in the form <alpha><digit>" << endl;
+	cout << "For example, A0" << endl << endl;
+
+	while (!board.isGameOver())
+	{
+		currentPlayer = static_cast<player>(turnNumber % 2);
+
+		if (currentPlayer == humanPlayer)
+		{
+			board.printBoard();
+			cout << endl << "Move: ";
+			cin >> move;
+
+			if (move.length() > 2 || !isalpha(move[0]) || toupper(move[0]) - 64 > BOARD_SIZE || 
+					!isdigit(move[1]) || (move[1] - 48) + 1 > BOARD_SIZE)
+			{
+				cout << "Make sure that your move is of the form <alpha><digit>" << endl;
+				cout << "For example, A0" << endl;
+			}
+			else
+			{
+				madeMove = true;
+			}
+
+			column = toupper(move[0]) - 65;
+			row = move[1] - 48;			
+			playerMove = BOARD_SIZE * row + column;
+
+			board.makeMove(playerMove, currentPlayer);			
+		}
+		else
+		{
+			playerMove = bot.play(board, currentPlayer);
+			board.makeMove(playerMove, currentPlayer);
+		}
+
+		turnNumber += 1;
+	}
+
+	if (currentPlayer == humanPlayer)
+	{
+		cout << "Congratulations! You are the Winner!" << endl;
+	}
+	else
+	{
+		cout << "Better luck next time!" << endl;
+	}
+}
